f1359e4955e5adb3e5d74eca2938ac1b684c111a
Armand - Jambon <armand.deffrennes@student.yncrea.fr>
2021-12-03 00:32:42 +0100

    New Curve Page with tons of modifications

---
+++ TP3/js/createExemple.js
@@ -0,0 +1,17 @@
+//First point
+inputX.value = 0;
+inputY.value = 0;
+confirmCoordEvent();
+
+//Second point
+inputX.value = 0;
+inputY.value = 1;
+confirmCoordEvent();
+
+//Third point
+inputX.value = 1;
+inputY.value = 1;
+confirmCoordEvent();
+
+inputX.value = "";
+inputY.value = "";
